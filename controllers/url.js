a4afb9c968e43e6fc08d6991ddce2118c2ba60fe
RandomThacker <randomthacker@gmail.com>
2024-03-26 12:53:10 +0530

    generate short id

--- controllers/url.js
+++ controllers/url.js
@@ -1,3 +1,19 @@
-async function handleGenerateNewShortUrl(req,res){
+const shortid = require("shortid");
+const URL = require("../models/url");
 
+async function handleGenerateNewShortUrl(req,res){
+    const body = req.body;
+    if(!body.url)
+        return res.status(400).json({error:"URL field is required"})
+    const shortID = shortid();
+
+    await URL.create({
+        shortId: shortID,
+        redirectURL: body.url,
+        visitHistory: []
+    })
+
+    return res.json({ id: shortID})
 }
+
+module.exports= {handleGenerateNewShortUrl}
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express")
+const urlRoute = require("./routes/url")
+const {connectToMongoDB} = require('./connect')
+
 const app = express()
 const PORT = 8000;
 
+app.use(express.json())
+
+connectToMongoDB('mongodb://localhost:27017/short-url')
+.then(()=>console.log("mongo db connected"))
+
+app.use("/url", urlRoute)
 
 app.listen(PORT, ()=>{
     console.log("APP IS LISTENING");
