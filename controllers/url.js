4bc7539c721fdae944bd3d8dcf748f52d273180c
RandomThacker <randomthacker@gmail.com>
2024-03-26 13:09:41 +0530

    url analytics

--- controllers/url.js
+++ controllers/url.js
@@ -1,19 +1,27 @@
 const shortid = require("shortid");
 const URL = require("../models/url");
 
-async function handleGenerateNewShortUrl(req,res){
-    const body = req.body;
-    if(!body.url)
-        return res.status(400).json({error:"URL field is required"})
-    const shortID = shortid();
+async function handleGenerateNewShortUrl(req, res) {
+  const body = req.body;
+  if (!body.url)
+    return res.status(400).json({ error: "URL field is required" });
+  const shortID = shortid();
 
-    await URL.create({
-        shortId: shortID,
-        redirectURL: body.url,
-        visitHistory: []
-    })
+  await URL.create({
+    shortId: shortID,
+    redirectURL: body.url,
+    visitHistory: [],
+  });
 
-    return res.json({ id: shortID})
+  return res.json({ id: shortID });
 }
 
-module.exports= {handleGenerateNewShortUrl}
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
+}
+module.exports = { handleGenerateNewShortUrl, handleGetAnalytics };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const urlRoute = require("./routes/url")
 const {connectToMongoDB} = require('./connect')
+const URL = require("./models/url")
 
 const app = express()
 const PORT = 8000;
@@ -11,6 +12,20 @@ connectToMongoDB('mongodb://localhost:27017/short-url')
 .then(()=>console.log("mongo db connected"))
 
 app.use("/url", urlRoute)
+app.get("/:shortId", async (req,res)=>{
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        "shortId": shortId
+    },{
+        $push:{
+            visitHistory: {
+                timestamp: Date.now()
+            }
+        }
+    })
+    res.redirect(entry.redirectURL)
+})
+
 
 app.listen(PORT, ()=>{
     console.log("APP IS LISTENING");
