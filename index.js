801845ab3ce60e89164d0e2f8174babd11b72010
RandomThacker <randomthacker@gmail.com>
2024-03-27 12:25:52 +0530

    home page ui ejs

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const express = require("express")
 const urlRoute = require("./routes/url")
 const {connectToMongoDB} = require('./connect')
 const URL = require("./models/url")
+const path = require("path")
+const staticRoute = require("./routes/staticRouter")
 
 const app = express()
 const PORT = 8000;
 
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.set("view engine", "ejs")
+app.set("views", path.resolve("./views"))
 
 connectToMongoDB('mongodb://localhost:27017/short-url')
 .then(()=>console.log("mongo db connected"))
 
 app.use("/url", urlRoute)
+app.use("/",staticRoute)
+
 app.get("/:shortId", async (req,res)=>{
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
